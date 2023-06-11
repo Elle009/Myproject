227c7ad327ee8228ff9252d21770d01a1e0098d8
Elle009 <bulgakovaeleonora@gmail.com>
2023-06-11 16:53:06 +0300

    Please review

---
+++ src/HW1.java
@@ -0,0 +1,12 @@
+public class HW1 {
+    public static int myMethod(int a, int b) {
+        int sum = a + b;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int sum = myMethod(3, 4);
+        System.out.println(sum);
+    }
+
+}
